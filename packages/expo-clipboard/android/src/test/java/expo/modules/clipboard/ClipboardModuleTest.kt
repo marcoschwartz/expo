da5aa956d0641cb523499693f5293c2787137e89
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-02-25 11:03:42 +0100

    Fix test

--- packages/expo-clipboard/android/src/test/java/expo/modules/clipboard/ClipboardModuleTest.kt
+++ packages/expo-clipboard/android/src/test/java/expo/modules/clipboard/ClipboardModuleTest.kt
@@ -24,6 +24,8 @@ import org.robolectric.annotation.Implementation
 import org.robolectric.annotation.Implements
 import org.robolectric.shadows.ShadowContextImpl
 
+private val clipboardUnavailableErrorCode = ClipboardUnavailableException().code
+
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P]) // API 28
 class ClipboardModuleTest {
@@ -97,7 +99,7 @@ class ClipboardModuleTest {
     val exception = runCatching { module.callFunction("getStringAsync") }.exceptionOrNull()
     assertNotNull(exception)
     assertTrue(exception is CodedException)
-    assertEquals(ERR_CLIPBOARD_UNAVAILABLE, (exception as CodedException).code)
+    assertEquals(clipboardUnavailableErrorCode, (exception as CodedException).code)
   }
 
   private val clipboardManager: ClipboardManager
