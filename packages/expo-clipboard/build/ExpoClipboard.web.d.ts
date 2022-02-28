e63fb2957d6745df4f9807d81d88a1812559baab
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-02-28 13:32:53 +0100

    Basic support for images

--- packages/expo-clipboard/build/ExpoClipboard.web.d.ts
+++ packages/expo-clipboard/build/ExpoClipboard.web.d.ts
@@ -1,8 +1,12 @@
+import { ClipboardImage, GetImageOptions } from './Clipboard.types';
 declare const _default: {
     readonly name: string;
     getStringAsync(): Promise<string>;
     setString(text: string): boolean;
     setStringAsync(text: string): Promise<boolean>;
+    getImageAsync(options: GetImageOptions): Promise<ClipboardImage | null>;
+    setImageAsync(base64image: string): Promise<void>;
+    hasImageAsync(): Promise<boolean>;
     addClipboardListener(): void;
     removeClipboardListener(): void;
 };
